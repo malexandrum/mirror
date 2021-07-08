a11ee9a697d823da14e48e0666921063398262f9
Alexandru MOLODOI <alexandrumm@hotmail.com>
2021-07-08 14:22:58 -0700

    added multithreading

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
 from uuid import uuid4
+from time import sleep
 import ssl
 
 PORT = 8083
@@ -11,7 +12,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         self.mirror()
-    
+
     def do_PUT(self):
         self.mirror()
 
@@ -24,28 +25,27 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.send_header('Set-Cookie', f"session={uuid4()}; SameSite=Lax;")
         self.end_headers()
         self.wfile.write(b'\n==> Request line:\n')
-        self.wfile.write(bytes(self.requestline + '\n', 'UTF-8'))            
+        self.wfile.write(bytes(self.requestline + '\n', 'UTF-8'))
         self.wfile.write(b'\n==> Headers:\n')
         self.wfile.write(self.headers.as_bytes())
-        if (self.command == "POST" or self.command == "PUT"):                
+        if (self.command == "POST" or self.command == "PUT"):
             cl = self.headers.get("Content-Length")
             if cl != "" and cl != None and int(cl) != 0:
                 self.wfile.write(b'\n==> Body:\n')
                 self.wfile.write(self.rfile.read(int(cl)))
-        
+
         self.wfile.write(b'\n')
-    
-server1 = HTTPServer(('', PORT), SimpleHTTPRequestHandler)
+
+server1 = ThreadingHTTPServer(('', PORT), SimpleHTTPRequestHandler)
 
 if SSL:
-    server1.socket = ssl.wrap_socket (server1.socket,
-            keyfile='./demokey.pem',
-            certfile='./democert.pem', server_side=True)
+    server1.socket = ssl.wrap_socket(server1.socket,
+                                     keyfile='./demokey.pem',
+                                     certfile='./democert.pem', server_side=True)
 
 
-try:    
+try:
     server1.serve_forever()
-except KeyboardInterrupt:    
+except KeyboardInterrupt:
     print("\nShutting down...")
     server1.socket.close()
-
